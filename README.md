f3759002d3f3611a63c8f1c8a4646fc6885c85fb
Kelly Mears <developers@tinypixel.dev>
2019-05-03 01:12:43 -0700

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Laravel Mix Astroturf
+
+Compile CSS-in-JSS pre-runtime with [Astroturf](https://github.com/4Catalyzer/astroturf) and PostCSS magika.
+
+## Install
+
+`npm install laravel-mix-astroturf --save-dev`
+
+`yarn add -D laravel-mix-astroturf`
+
+## Usage
+
+```js
+require("laravel-mix-astroturf")
+
+mix.astroturf()
+```
---
+++ index.js
@@ -0,0 +1,74 @@
+const mix = require('laravel-mix')
+
+class Astroturf {
+  /**
+   * The optional name to be used when called by Mix.
+   * Defaults to the class name, lowercased.
+   *
+   * Ex: mix.example();
+   *
+   * @return {String|Array}
+   */
+  name() {
+    return 'astroturf'
+  }
+
+  /**
+   * All dependencies that should be installed by Mix.
+   *
+   * @return {Array}
+   */
+  dependencies() {
+    this.requiresReload = `
+      Dependencies have been installed. Please run again.
+    `
+
+    return ['astroturf', 'react', 'postcss-nested']
+  }
+
+  /**
+   * Override the generated webpack configuration.
+   *
+   * @param  {Object} webpackConfig
+   * @return {void}
+   */
+  webpackConfig(webpackConfig) {
+    webpackConfig = {
+      module: {
+        rules: [
+          {
+            test: /\module\.scss$/,
+            use: [{
+              loader: 'style-loader'
+            }, {
+              loader: 'astroturf/css-loader',
+            }, {
+              loader: 'sass-loader',
+              options: {
+                implementation: require("sass")
+              }
+            }],
+          },
+          {
+            test: /\.js?$/,
+            use: [
+              'babel-loader',
+              {
+                loader: 'astroturf/loader',
+                options: { extension: '.module.scss' },
+              },
+            ],
+          },
+        ],
+      }
+    }
+  }
+
+  babelConfig() {
+    return {
+      plugins: ['astroturf/plugin']
+    }
+  }
+}
+
+mix.extend('astroturf', new Astroturf())
